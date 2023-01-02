70123c81626e5e4ba57db4807e9040b488fd6ce2
bristol <oro.eto.bristol@gmail.com>
2023-01-02 18:49:43 +0200

    Initial Commit

---
+++ Right on target/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Right on target
+//
+//  Created by Apple M1 on 01.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
