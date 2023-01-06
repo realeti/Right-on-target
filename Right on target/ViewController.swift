06788465dbb770c4d8edd46ce560be4c86a872db
bristol <oro.eto.bristol@gmail.com>
2023-01-06 23:27:33 +0200

    added new text labels for helping, added update score and rounds for button, edited final round message

--- Right on target/ViewController.swift
+++ Right on target/ViewController.swift
@@ -11,12 +11,17 @@ class ViewController: UIViewController {
     
     @IBOutlet var slider: UISlider!
     @IBOutlet var label: UILabel!
+    @IBOutlet var roundNum: UILabel!
+    @IBOutlet var button: UIButton!
     
     // загаданное число
     var number: Int = 0
     
     // раунд
     var round: Int = 0
+    
+    // максимальное число раундов
+    let roundMax: Int = 5
 
     // сумма очков за раунд
     var points: Int = 0
@@ -29,14 +34,16 @@ class ViewController: UIViewController {
     @IBAction func checkNumber() {
         // если игра только начинается
         if self.round == 0 {
-            // генерируем случайное число
-            self.number = Int.random(in: 1...50)
-            
-            // передаем значение случайного числа в label
-            self.label.text = String(self.number)
+            // изменяем текст кнопки
+            self.button.titleLabel?.font = UIFont(name: "Helvetica Neue", size: 20)
+            self.button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 50)
+            self.button.setTitle("Проверить", for: UIControl.State.normal)
             
             // устанавливаем счетчик раундов на 1
             self.round = 1
+            
+            // генерируем случайное число и передаем значение случайного числа в label
+            generateNewRound()
         }
         else {
             // получаем значение на слайдере
@@ -53,25 +60,39 @@ class ViewController: UIViewController {
                 self.points += 50
             }
             
-            if self.round == 5 {
+            if self.round == roundMax {
                 // выводим информационное окно с результатами игры
                 let alert = UIAlertController(
                     title: "Игра окончена",
-                    message: "Вы заработали \(self.points) очков!",
+                    message: "Вы заработали \(self.points) очков из 250 возможных!",
                     preferredStyle: .alert)
                 alert.addAction(UIAlertAction(title: "Начать заново", style: .default, handler: nil))
                 
-                self.present(alert, animated: true, completion: nil)
                 self.round = 1
                 self.points = 0
+                generateNewRound()
+                self.present(alert, animated: true, completion: nil)
             }
             else {
-                self.round += 1
+                let alert = UIAlertController(
+                    title: "Итоги раунда №\(round):",
+                    message: "Вы заработали \(self.points) очков! Вы поставили ползунок на число: \(numSlider)",
+                    preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Далее", style: .default, handler: {
+                    _ in
+                    self.round += 1
+                    generateNewRound()
+                }))
+                
+                self.present(alert, animated: true, completion: nil)
             }
+        }
+        func generateNewRound() {
             // генерируем случайное число
             self.number = Int.random(in: 1...50)
             // передаем значение случайного числа в label
             self.label.text = String(self.number)
+            self.roundNum.text = String(self.round)
         }
     }
 
