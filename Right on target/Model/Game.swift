12772f8baddeead69b1b6d52963c005ded6669c1
bristol <oro.eto.bristol@gmail.com>
2023-01-22 16:00:41 +0200

    add new class Game, refactoring code

---
+++ Right on target/Model/Game.swift
@@ -0,0 +1,99 @@
+//
+//  Game.swift
+//  Right on target
+//
+//  Created by Apple M1 on 17.01.2023.
+//
+
+import UIKit
+
+protocol GameProtocol {
+    // Количество заработанных очков
+    var score: Int { get }
+    
+    // Загаданное значение
+    var currentSecretValue: Int { get }
+    
+    // Проверяем, закончена ли игра
+    var isGameEnded: Bool { get }
+    
+    // Начинаем новую игру с первого раунда
+    func restartGame()
+    
+    // Начинаем новый раунд (обновляем загаданное число)
+    func startNewRound()
+    
+    // Сравниваем переданное значние с загаданным и начисляем очки
+    func calculateScore(with value: Int)
+}
+
+class Game: GameProtocol {
+    var score: Int = 0
+    var roundScore: Int = 0
+    
+    // Минимальное загаданное значение
+    private var minSecretValue: Int
+    
+    // Максимальное загаданное значение
+    private var maxSecretValue: Int
+    
+    var currentSecretValue: Int = 0
+    
+    // Количество раундов
+    private var lastRound: Int
+    var currentRound: Int = 1
+    
+    var isGameEnded: Bool {
+        if currentRound >= lastRound {
+            return true
+        }
+        
+        return false
+    }
+    
+    init?(startValue: Int, endValue: Int, rounds: Int) {
+        // Стартовое значение для выбора случайного числа не может быть больше конечного
+        guard startValue <= endValue else {
+            return nil
+        }
+        
+        minSecretValue = startValue
+        maxSecretValue = endValue
+        lastRound = rounds
+        currentSecretValue = self.getNewSecretValue()
+    }
+    
+    func restartGame() {
+        currentRound = 0
+        score = 0
+        startNewRound()
+    }
+    
+    func startNewRound() {
+        currentSecretValue = self.getNewSecretValue()
+        currentRound += 1
+    }
+    
+    // Загадать и вернуть новое случайное значение
+    private func getNewSecretValue() -> Int {
+        (minSecretValue...maxSecretValue).randomElement()!
+    }
+    
+    // Подсчитывает количество очков
+    func calculateScore(with value: Int) {
+        roundScore = 0
+        
+        if value > currentSecretValue {
+            score += 50 - value + currentSecretValue
+            roundScore += 50 - value + currentSecretValue
+        }
+        else if value < currentSecretValue {
+            score += 50 - currentSecretValue + value
+            roundScore += 50 - currentSecretValue + value
+        }
+        else {
+            score += 50
+            roundScore += 50
+        }
+    }
+}
